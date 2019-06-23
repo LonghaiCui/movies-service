f1510d11ce05da92f6922d25b3e65e1533125156
nana <nana@nanas-MacBook-Pro.local>
2019-06-23 00:15:38 -0600

    Added integration test cases for searching movie by year flow

--- src/test/java/com/longhai/moviesservice/acceptance/MoviesIntegrationTest.java
+++ src/test/java/com/longhai/moviesservice/acceptance/MoviesIntegrationTest.java
@@ -29,7 +29,6 @@ public class MoviesIntegrationTest {
     private static ObjectMapper objectMapper;
 
     private String jsonRequest;
-//    private User user;
 
     @Before
     public void setUp() throws Exception {
@@ -38,29 +37,148 @@ public class MoviesIntegrationTest {
 
         objectMapper = new ObjectMapper();
         jsonRequest = getJSON("/userRequest.json");
-//        user = objectMapper.readValue(jsonRequest, User.class);
     }
 
 
+    /*
+        Given a GET request is received in /movies endpoint
+        When the queryString is year and the value is valid (from 1900 to 2100)
+        Then the service should return the movie list in that year
+    */
     @Test
-    public void test() {
-        Response response =
-                given()
-                        .header("sessionId", "session123")
-                        .when()
-                        .get("/movies")
-                        .then()
-                        .statusCode(201)
-                        .extract()
-                        .response();
+    public void testSearchMoviesByYear_whenYearIsValid_thenReturnMoviesInThatYear() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("year", 2018)
+                .get("/movies")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
 
-        UserResponse userResponse = response.getBody().as(UserResponse.class);
+    }
 
-        assertEquals(UserResponse.builder()
-                        .status("201")
-                        .message("User is created successfully with email longhaicui.work@gmail.com")
-                        .build(),
-                userResponse);
+    /*
+        Given a GET request is received in /movies endpoint
+        When the queryString is year and the value is not valid (before 1900 or after 2100)
+        Then the service should return error response with 400 status code
+
+    */
+    @Test
+    public void testSearchMoviesByYear_whenYearIsNotValid_thenReturn400() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("year", 9999)
+                .get("/movies")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+
+    }
+
+    /*
+        Given a GET request is received in /movies endpoint
+        When the queryStrings are "from" and "to"
+        And the values are valid ("from" is bigger than 1900 and "to" is smaller than 2100)
+        Then the service should return the movie list in that year
+    */
+    @Test
+    public void testSearchMoviesByYear_whenQueryStringsAreValidFromAndTo_thenReturn400() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("from", 2012)
+                .param("to", 2013)
+                .get("/movies")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+
+    }
+
+    /*
+        Given a GET request is received in /movies endpoint
+        When the queryStrings both "from" and "to" are present
+        And the values are not valid ("from" is smaller than 1900 or "to" is bigger than 2100)
+        Then the service should return error response with 400 status code
+    */
+    @Test
+    public void testSearchMoviesByYear_whenQueryStringsFromAndToAreNotValid_thenReturn400() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("from", 1000)
+                .param("to", 3000)
+                .get("/movies")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    /*
+        Given a GET request is received in /movies endpoint
+        When the queryStrings "from" is bigger than "to"
+        Then the service should return error response with 400 status code
+    */
+    @Test
+    public void testSearchMoviesByYear_whenQueryStringsFromIsBiggerThanTo_thenReturn400() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("from", 2020)
+                .param("to", 2000)
+                .get("/movies")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    /*
+        Given a GET request is received in /movies endpoint
+        When only one of "from" and "to" is present
+        Then the service should return error response with 400 status code
+    */
+    @Test
+    public void testSearchMoviesByYear_whenOnlyFromIsPresent_thenReturn400() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .param("from", 1000)
+                .get("/movies")
+                .then()
+                .statusCode(400)
+                .extract()
+                .response();
+    }
+
+    /*
+        Given a GET request is received in /movies endpoint
+        When the none of the queryString "year", "from" or "to" is present
+        Then the service should return the movie lists in default year 2016
+    */
+    @Test
+    public void testSearchMoviesByYear_whenYearIsMissing_thenReturnDefault2016() {
+        Response getResponse = given()
+                .header("sessionId", "session123")
+                .when()
+                .contentType("application/json")
+                .get("/movies")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
     }
 
     private String getJSON(String path) throws Exception {
